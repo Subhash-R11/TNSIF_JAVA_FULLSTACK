e09afdea3faf4fdf9b762efd55fd5f54761e0a17
Subhash R <subhashr2911@gmail.com>
2025-09-28 19:45:31 +0530

    Add files via upload

---
+++ inheritance/Demo.java
@@ -0,0 +1,12 @@
+package com.tns.inheritance;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		Person p=new Person("Ajay",24,"Bangalore");
+		System.out.println(p);
+		Student s=new Student("Shashi",22,"Mumbai",102,"Medical","MBBS");
+		System.out.println(s);
+	}
+
+}
